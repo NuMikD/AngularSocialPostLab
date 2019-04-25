a04ed7f62a004531073328e446b39f7b53123b36
Michael Daniels <mdaniel@oakparkschools.net>
2019-04-25 13:25:58 -0400

    fixed post box issues_added color to title_clean up code

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -13,26 +13,12 @@ titleInput: string;
 @Input() postList: Posting;
 @Output() delete = new EventEmitter<any>();
 
-
-isRounded: boolean = true;
-isPadded: boolean = true; 
-isWidths: boolean = true; 
 isflexElem: boolean = true;
 isflexRow: boolean = true;
-isStrike: boolean = false;
-isWide: boolean = true;
-isMargin: boolean = true;
-// delete: boolean = null;
 
 classList: Object = {
-  round: this.isRounded,
-  padding: this.isPadded,
-  widths: this.isWidths,
   flexing: this.isflexElem,
-  flexrow: this.isflexRow,
-  strike: this.isStrike,
-  componentWidths: this.isWide,
-  componentMargins: this.isMargin
+  flexrow: this.isflexRow
  };
 
  erasePost = () => {
